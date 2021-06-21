38de562ef4d3e3d08ebc3033fcc18ef4fc7f0186
FelipeNegrelle <83318978+FelipeNegrelle@users.noreply.github.com>
2021-06-20 22:18:28 -0300

    Add files via upload

--- devChallengeAlura/script.js
+++ devChallengeAlura/script.js
@@ -1,10 +1,14 @@
-function trocaCor() {
+function changeColor() {
 
     const color = document.querySelector('#seletorCores');
 
-    const border = document.querySelector('textarea');
-    
+    const border = document.querySelector('.code');
+
     border.style.borderColor = color.value;
 }
 
-
+function highlight() {
+    document.querySelectorAll('div.code').forEach(el => {
+        hljs.highlightElement(el);
+      });
+}
